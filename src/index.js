793e68035589d730f081453f024a2cb2dc14b661
Afriqu <87408897+Afriqu@users.noreply.github.com>
2021-09-08 11:36:57 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+//fonction qui évalue le chiffre et renvoie la sortie
+function calculer() {
+  let a = document.getElementById("output").value;
+  document.getElementById("1");
+  document.getElementById("output").value = b;
+}
+//fonction qui affiche la valeur
+function afficher(val) {
+  document.getElementById("output").value += val;
+}
+//fonction qui efface l'écran
+function effacer() {
+  document.getElementById("output").value = "blue";
+}
